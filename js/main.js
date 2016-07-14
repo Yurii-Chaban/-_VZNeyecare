7c3bf9a789f7f451b9fae83369458b126444f565
YuriyChaban <shuyterrush@gmail.com>
2016-07-14 22:05:32 +0300

    create 2 new page, fix link in all page

--- js/main.js
+++ js/main.js
@@ -119,6 +119,16 @@ $(function () {
         }
     });
 });
+// $(function () {
+//     $('.banner-1 .services-sign a').each(function () {
+//         var location = window.location.href;
+//         var link = this.href;
+//         var result = location.match(link);
+//         if(result) {
+//         $(this).parent("li").addClass('services-active');
+//         }
+//     });
+// });
 /*-----------------------------------------*/
 /*-------------for contact page form--------*/
 $(document).ready(function($) {
